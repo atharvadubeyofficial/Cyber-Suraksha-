6524cf4d2b39eb3c4cc0499b394f22f29c65b537
atharvadubeyofficial <24f3003562@ds.study.iitm.ac.in>
2025-11-22 22:35:32 +0530

    Add Progress component for displaying progress bar

---
+++ frontend/src/components/progress.jsx
@@ -0,0 +1,10 @@
+export function Progress({ value }) {
+  return (
+    <div className="w-full bg-gray-200 rounded-full h-2 overflow-hidden">
+      <div
+        className="bg-blue-600 h-2 transition-all"
+        style={{ width: `${value}%` }}
+      ></div>
+    </div>
+  );
+}
