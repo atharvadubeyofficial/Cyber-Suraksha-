f7e0d3f1cb8471abde17efa04a533be44dc6c230
hardik <hnoob2992@GMAIL.COM>
2025-11-23 00:48:46 -0800

    Update PhishingSimulation.jsx

--- frontend/src/simulations/PhishingSimulation.jsx
+++ frontend/src/simulations/PhishingSimulation.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { useState } from 'react';
 import { Button } from '../components/button';
 import { AlertTriangle, CheckCircle, Mail, ExternalLink, Paperclip } from 'lucide-react';
