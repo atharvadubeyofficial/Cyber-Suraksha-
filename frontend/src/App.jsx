615ebe21f6d0278548add1890d80a136c506d7a6
hardik <hnoob2992@GMAIL.COM>
2025-11-22 23:45:54 -0800

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,9 +2,9 @@ import { useState, useEffect } from "react";
 import "./App.css"; // FIXED for Vercel builds
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import LandingPage from "./pages/LandingPage";
-import Dashboard from "./pages/Dashboard";
-import Simulator from "./pages/Simulator";
+import LandingPage from "./pages/LandingPage.jsx";
+import Dashboard from "./pages/Dashboard.jsx";
+import Simulator from "./pages/Simulator.jsx";
 import ChatWidget from "./components/ChatWidget";
 import { Toaster } from "./components/ui/sonner";
 
