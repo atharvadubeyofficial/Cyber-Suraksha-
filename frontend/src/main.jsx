298ed0f9fa70fd81aba1c4e3eb9e6604e348eb53
atharvadubeyofficial <24f3003562@ds.study.iitm.ac.in>
2025-11-24 02:26:36 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,8 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.js";     // YES this is correct
-import "./index.css";
-import "./App.css";
+import App from "./App.jsx";
+import "./index.css";   // <-- TAILWIND ENABLED
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
